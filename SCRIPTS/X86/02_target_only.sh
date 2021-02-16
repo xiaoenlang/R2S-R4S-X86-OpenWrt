cded168393f17bdc930a3453ac0fdd92e7dae213
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-02-16 11:25:57 +0800

    Update 02_target_only.sh

--- SCRIPTS/X86/02_target_only.sh
+++ SCRIPTS/X86/02_target_only.sh
@@ -5,13 +5,13 @@ sed -i '/openssl/d' ./package/lean/lean-translate/files/zzz-default-settings
 sed -i '/banirq/d' ./package/lean/lean-translate/files/zzz-default-settings
 sed -i '/rngd/d' ./package/lean/lean-translate/files/zzz-default-settings
 
-until :
+<<'COMMENT'
 #Vermagic
 latest_version="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+.tar.gz" |sed -n '/21/p' |sed -n 1p |sed 's/v//g' |sed 's/.tar.gz//g')"
 wget https://downloads.openwrt.org/releases/${latest_version}/targets/x86/64/packages/Packages.gz
 zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' > .vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
-done
+COMMENT
 
 #预配置一些插件
 cp -rf ../PATCH/X86/files ./files
